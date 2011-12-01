570bca9b07fd6dd9d2c68242df41f3337d9588c5
Cameron Johnston <cwjohnston@gmail.com>
2011-12-01 15:26:24 -0700

    Including build-essential in cookbook dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ license "Apache 2.0"
 description "Installs and configures Alice - an Altogether Lovely Internet Chatting Experience"
 version "0.1.0"
 
-%w{ git runit perl }.each do |cb|
+%w{ build-essential git runit perl }.each do |cb|
   depends cb
 end

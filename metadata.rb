099ba00637cc790a325f732e5bdc9e842b308d2b
Cameron Johnston <cameron@needle.com>
2012-08-20 11:56:14 -0600

    adding varnish cookbook to metadata and berks

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ license "Apache 2.0"
 description "Installs and configures Alice - an Altogether Lovely Internet Chatting Experience"
 version "0.2.0"
 
-%w{ build-essential git runit perl database }.each do |cb|
+%w{ build-essential git runit perl database varnish }.each do |cb|
   depends cb
 end

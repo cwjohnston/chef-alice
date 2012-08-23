fdb7dfd8d806719a4bb25425cd45d2e6705aece1
Cameron Johnston <cameron@needle.com>
2012-08-23 11:57:09 -0600

    attempting to fix mysql user/database setup errors

--- recipes/catlady.rb
+++ recipes/catlady.rb
@@ -19,7 +19,7 @@ end
 
 catlady_config "#{node[:catlady][:root]}/shared/etc/config.json"
 
-mysql_config = {:host => node[:catlady][:db][:hostname], :username => node[:catlady][:db][:username], :password => node[:catlady][:db][:password]}
+mysql_config = {:host => node[:catlady][:db][:hostname], :username => 'root', :password => node[:mysql][:server_root_password]}
 
 execute "initial SQL import" do
   command "mysql --user=#{node[:catlady][:db][:username]} --password=#{node[:catlady][:db][:password]} --host=#{node[:catlady][:db][:hostname]} #{node[:catlady][:db][:name]} < #{node[:catlady][:root]}/current/catlady.sql"
@@ -66,13 +66,6 @@ mysql_database node[:catlady][:db][:name] do
   connection mysql_config
   action :create
   notifies :run, "execute[initial SQL import]", :immediately
-end
-
-mysql_database_user node[:catlady][:db][:username] do
-  connection mysql_config
-  password node[:catlady][:db][:password]
-  action :create
-  not_if { node[:catlady][:db][:username] == "root" }
 end
 
 mysql_database_user node[:catlady][:db][:username] do

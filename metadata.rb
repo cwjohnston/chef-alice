2673bd4948a8de4300240c630be1985f677cca4c
Cameron Johnston <cameron@needle.com>
2012-11-20 13:22:13 -0700

    bumping cookbook version to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer "Cameron W Johnston"
 maintainer_email "cwjohnston@gmail.com"
 license "Apache 2.0"
 description "Installs and configures Alice - an Altogether Lovely Internet Chatting Experience"
-version "0.2.0"
+version "0.2.1"
 
 %w{ build-essential git runit perl database varnish }.each do |cb|
   depends cb

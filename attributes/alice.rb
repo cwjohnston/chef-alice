6535e55b403316c14e7876d755478745be3ea10d
Cameron Johnston <cameron@needle.com>
2013-07-23 08:01:02 -0600

    access attributes in a consistent fashion

--- attributes/alice.rb
+++ attributes/alice.rb
@@ -1,9 +1,9 @@
 # alice
-default.alice[:repo] = "https://github.com/leedo/alice.git"
-default.alice[:revision] = "HEAD"
-default.alice[:root] = "/opt/alice"
-default.alice[:ip] = "0.0.0.0"
-default.alice[:port] = "8000"
-default.alice[:user] = "alice"
-default.alice[:require_auth] = false
-default.alice[:run_standalone] = true
+default[:alice][:repo] = "https://github.com/leedo/alice.git"
+default[:alice][:revision] = "HEAD"
+default[:alice][:root] = "/opt/alice"
+default[:alice][:ip] = "0.0.0.0"
+default[:alice][:port] = "8000"
+default[:alice][:user] = "alice"
+default[:alice][:require_auth] = false
+default[:alice][:run_standalone] = true

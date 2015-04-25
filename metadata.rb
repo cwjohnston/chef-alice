8bd15390cc67178c0041f335aee22ad101701ce4
Cameron Johnston <cameron@rootdown.net>
2015-04-25 15:58:09 -0600

    unloop dependencies in metadata

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,12 @@ maintainer "Cameron W Johnston"
 maintainer_email "cwjohnston@gmail.com"
 license "Apache 2.0"
 description "Installs and configures Alice - an Altogether Lovely Internet Chatting Experience"
-version "0.3.4"
+version "0.3.5"
 
-%w{ build-essential git perl database varnish }.each do |cb|
-  depends cb
-end
 
-depends 'runit', '~> 1.1.6'
+depends 'build-essential'
+depends 'git'
+depends 'perl'
+depends 'database'
+depends 'varnish'
+depends 'runit', '~> 1.1'

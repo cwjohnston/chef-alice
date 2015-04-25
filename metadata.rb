91c1d21f4e059525833b5437587fb5ad3e1f4bd3
Cameron Johnston <cameron@rootdown.net>
2015-04-25 15:55:09 -0600

    bump version to 0.3.5 for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer "Cameron W Johnston"
 maintainer_email "cwjohnston@gmail.com"
 license "Apache 2.0"
 description "Installs and configures Alice - an Altogether Lovely Internet Chatting Experience"
-version "0.3.3"
+version "0.3.4"
 
 %w{ build-essential git perl database varnish }.each do |cb|
   depends cb
